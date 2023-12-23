fd065c9750b413e3f1a87937395e1bdfd6803bb1
Gagan <business@devgagan.in>
2023-12-23 13:23:13 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -18,7 +18,7 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 API_ID = "26075120" #config("API_ID", default=None, cast=int)
 API_HASH = "1fda88a5d1de46058a4791c78bce198e" #config("API_HASH", default=None)
 BOT_TOKEN = "6468925244:AAGOodGA2yOqZ_gtBATrmQvLpI1Aisl8VuI" #config("BOT_TOKEN", default=None)
-SESSION = "BAASAviVFV5gxSg1-gIgSh0hMCCevs7yVcaAdK-UvQPZSqgzqTPWthWGRnaoMqZsE4jSAR9UHAiJO3qZMWzt2TRFDzLyY1gmK0Q5xcTnmP0-3OuYbCPObUpjfM5oc8z_XBgaCNjwr69I7PtzFp8ndu_fRcs4AZpHl654Vjh_KoJt7bWh1hsa4XeV65SDF2-Zr7gKk-mRTGWlFQIFybPxWmrYgf_GcI1KBv8QvQYB6fdaZ-N2oVlxUCd35uKUGzKBV_-IgGbEeYGIlf-5-TmMyTKAoA0R5yRxiAMwyNvx-06x1ta94DU5WFN91o_H7x9qBRVd_iLFO1EBgA75BeNF0O0kAAAAAYSLfM0A" #config("SESSION", default=None)
+SESSION = "BAC-G_qTYu_zrzHElVzpoCxXGsQF1P8RGezMvT81P_m2ALxhZEVVva2Oq6p8oce6PyMB-QyscO-2KuS7af4eGYzJ9FstnYFApmsTmS97A7ObXoI8ljO21fDS2EfgxWTREZ6SXhG5QD2oELLgg53OrFCSD4zqKucuvovvHQGrKs_l6eWCbu1XPweV-2KqzeULv-veK6hh9sF8hTrufext-HPH-bSGwSc84dvDAi6el_dLyvNmO7sR6yE8IYFFLXsUl24Fh8nuvhYC0-N-WwtGdRuCG1Grc27YlzyVHlAWvdyBG_DA2qPniYAsYt-m9e8Tx3ptZHS2sFBSe64jXv15GdP5AAAAAYSLfM0A" #config("SESSION", default=None)
 FORCESUB = "dev_gagan" #config("FORCESUB", default=None)
 AUTH = "6876018655" #config("AUTH", default=None)
 SUDO_USERS = []
